ca15cab3d9f7a22fe9fc7da3ba4d2a8e13910ea3
ebenezer.chelliah <egchelliah@gmail.com>
2022-08-08 23:09:24 -0600

    Got it workign with cascading operations

--- calculator.js
+++ calculator.js
@@ -14,10 +14,12 @@ let totals = []
 let result = 0
 let currentOperator = ''
 
-
+//Listen for any button clicks
 allButtons.addEventListener('click', (event) =>{
     const buttonClick = event.target.nodeName === 'BUTTON'
+    // Store the value of the number, and display it in the window
     if (event.target.dataset.type === 'number'){
+        console.log('value of x at the start of the number click is ' + x)
         let addNumber = event.target.value
         x += addNumber
 
@@ -34,12 +36,21 @@ allButtons.addEventListener('click', (event) =>{
         // return newX
     }
 
+    // Store the operator value
+    // Append the value of x to the array
     if (event.target.dataset.type === 'operator'){
         if(result !==0) {
-            x = result
-            console.log('Assing new x from the previous result ')
-        }
+            console.log('Making sure this block of code is hit when you hit the operator after a result')
+            totals.push(currentOperator)
+            totals.push(result)
+            console.log('Assing new x from the previous equals sign and a new operator')
+            console.log('The new array is ' + totals)
+            result = 0
+        } 
+        
+    
         console.log('The array when this operator was hit is ' + totals)
+
         if(totals.length===0){
         currentOperator = event.target.value
         totals.push(currentOperator)
@@ -56,37 +67,37 @@ allButtons.addEventListener('click', (event) =>{
         else if (totals.length===2){
             totals.push(x)
             console.log('The operator was hit again in a row ' + totals)
-            result = operate(totals[0], parseInt(totals[1]),parseInt(totals[2]))
+            doubleOperator = operate(totals[0], parseInt(totals[1]),parseInt(totals[2]))
             console.log('The result of that double operator was ' + result)
+            totals = []
             currentOperator = event.target.value 
-            history.textContent = result + ' ' + currentOperator
+            history.textContent = doubleOperator + ' ' + currentOperator
             display.textContent = ''
-            totals=[]
-            totals=[currentOperator,result]
-            x=''
+            totals=[currentOperator, doubleOperator]
+            console.log('At the end of the second operator if block, the array of totals is ' + totals)       
         }
         
 
-        // x = 0
+        x = ''
         
     }
 
     if (event.target.dataset.type === 'equal'){
         totals.push(x)
-        console.table('The new array is ' + totals)
+        console.table('The new array at the beginning of the equals function is ' + totals)
         console.log('The equals was pushed ' + event.target.value)
-
-        console.log('The first number is ' + totals[1])
-        console.log('The second number is ' + totals[2])
-        console.log('The operator was ' + totals[0])
 
         history.textContent += ' ' + totals[2]
 
         result = operate(totals[0], parseInt(totals[1]),parseInt(totals[2]))
         display.textContent = result
         totals = []
-        x = result
+        x = ''
+        currentOperator = ''
         console.log('New array after equals is ' + totals)
+        console.log('The value of x after the equals is ' + x)
+        console.log('The value of result at the end of the equals is ' + result)
+        console.log('The value of the operator at the end of the equals is ' + currentOperator)
 
         // console.log('The result of ' + totals[1] + ' ' + totals[0] + ' ' + totals[2] + ' was ' + final)
     }
@@ -113,7 +124,8 @@ function operate(op, x, y){
     if (op === '+'){
         finalNum = sum(x,y)
     } else if (op === '-'){
-        finalNum = substract(x,y)
+        console.log('Hit the else statement in the operate function because of the -')
+        finalNum = subtract(x,y)
     } else if (op === 'x'){
         finalNum = multiply(x,y)
     } else if (op === '/'){
@@ -127,21 +139,13 @@ function sum(x, y){
     return x + y
 }
 
-function substract(x,y){
+function subtract(x,y){
+    console.log('hit the subtract function')
     return x - y
 }
 
 function multiply(x,y){
-    console.log(x)
-    console.log(y)
-    let newThing = x*y
-    console.log(newThing)
-    typeof(newThing)
-    return newThing
-    console.log('Getting this console log from multiply function ' + x*y)
-    // console.log(typeOf(x*y))
-    //return x*y This does not work. Returns undefined
-
+    return x*y
 }
 
 function divide(x,y){
